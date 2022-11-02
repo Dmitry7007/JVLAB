ebbbd01e3ef84dc12eea765f2f1832a1ad89e2db
Dmitry7007 <99890690+Dmitry7007@users.noreply.github.com>
2022-11-02 11:21:10 +0700

    02.11

--- src/Zadanie_2.java
+++ src/Zadanie_2.java
@@ -1,14 +1,22 @@
 package lab4;
+import java.util.Scanner;
 
 public class Zadanie_2 {
    static double y = 0;
+   System.out.println("Введите знаменатель последнего слагаемого суммы: ");
+   int n = (new Scanner (System.in)).nextInt();
+   if (n <= 0) {
+      System.out.println("Введите натуральное число! ");
+   }
+   
    public static void main(String[] args) {
-       for (int i = 1; i <= 10000; i++) {
+       for (int i = 1; i <= n; i++) {
            if (i % 2 == 0) {
                y -= 1/i;
            }
            y += 1/i;
        }
+
        System.out.println("Значение выражения равно: " + y);
    }
 }
